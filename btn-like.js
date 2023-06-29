74008ad80a94b1813706eece01681ed35626c050
fattaah99 <121369097+fattaah99@users.noreply.github.com>
2023-06-29 10:58:32 +0700

    Add files via upload
    Simple website food recipe using HTML, CSS, and JavaScript

---
+++ btn-like.js
@@ -0,0 +1,203 @@
+function jmlResep1() {
+  var a = 150;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like1").innerHTML = jumlah;
+}
+function jmlResep2() {
+  var a = 100;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like2").innerHTML = jumlah;
+}
+function jmlResep3() {
+  var a = 83;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like3").innerHTML = jumlah;
+}
+function jmlResep4() {
+  var a = 90;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like4").innerHTML = jumlah;
+}
+function jmlResep5() {
+  var a = 88;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like5").innerHTML = jumlah;
+}
+function jmlResep6() {
+  var a = 30;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like6").innerHTML = jumlah;
+}
+function jmlResep7() {
+  var a = 110;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like7").innerHTML = jumlah;
+}
+
+function jmlResep8() {
+  var a = 25;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like8").innerHTML = jumlah;
+}
+
+function jmlResep9() {
+  var a = 32;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like9").innerHTML = jumlah;
+}
+
+function jmlResep10() {
+  var a = 40;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like10").innerHTML = jumlah;
+}
+
+function jmlResep11() {
+  var a = 15;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like11").innerHTML = jumlah;
+}
+
+function jmlResep12() {
+  var a = 35;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like12").innerHTML = jumlah;
+}
+
+function jmlResep13() {
+  var a = 20;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like13").innerHTML = jumlah;
+}
+
+function jmlResep14() {
+  var a = 85;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like14").innerHTML = jumlah;
+}
+
+function jmlResep15() {
+  var a = 42;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like15").innerHTML = jumlah;
+}
+
+function jmlResep16() {
+  var a = 37;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like16").innerHTML = jumlah;
+}
+
+function jmlResep17() {
+  var a = 50;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like17").innerHTML = jumlah;
+}
+
+function jmlResep18() {
+  var a = 49;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like18").innerHTML = jumlah;
+}
+
+function jmlResep19() {
+  var a = 22;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like19").innerHTML = jumlah;
+}
+
+function jmlResep20() {
+  var a = 19;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like20").innerHTML = jumlah;
+}
+
+function jmlResep21() {
+  var a = 31;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like21").innerHTML = jumlah;
+}
+
+function jmlResep22() {
+  var a = 31;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like22").innerHTML = jumlah;
+}
+
+function jmlResep23() {
+  var a = 28;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like23").innerHTML = jumlah;
+}
+
+function jmlResep24() {
+  var a = 80;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like24").innerHTML = jumlah;
+}
+
+function jmlResep25() {
+  var a = 39;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like25").innerHTML = jumlah;
+}
+
+function jmlResep26() {
+  var a = 22;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like26").innerHTML = jumlah;
+}
+
+function jmlResep27() {
+  var a = 17;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like27").innerHTML = jumlah;
+}
+
+function jmlResep28() {
+  var a = 19;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like28").innerHTML = jumlah;
+}
+
+function jmlResep29() {
+  var a = 24;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like29").innerHTML = jumlah;
+}
+
+function jmlResep30() {
+  var a = 32;
+  var jumlah;
+  jumlah = a + 1;
+  document.getElementById("like30").innerHTML = jumlah;
+}
